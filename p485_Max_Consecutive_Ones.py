aa2aeef75cf266bbef4391bbc2ad71ae1a3a3216
Bo Zhou <bzhou2@ualberta.ca>
2017-01-25 20:24:06 -0700

    344 done

---
+++ p485_Max_Consecutive_Ones.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_sub = 0
+        sub = 0
+        for i in nums:
+           if i == 1: 
+               sub += 1
+               max_sub = max(sub, max_sub)
+           else: sub = 0
+        return max_sub
