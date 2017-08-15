65cdd7583189097713f1e417b9e6b37a454ddc5d
Bo Zhou <bo.zhou@guest-tek.com>
2017-08-14 23:11:17 -0600

    finish question 461

--- p461_hamming_distance.py
+++ p461_hamming_distance.py
@@ -11,4 +11,19 @@ class Solution(object):
             sum += binary_sum % 2
             binary_sum //= 2
         return sum
-        
+
+
+class Solution2(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        result = 0
+        while x != 0 or y != 0:
+            result += 1 if x % 2 != y % 2 else 0
+            x = x >> 1
+            y = y >> 1
+
+        return result
