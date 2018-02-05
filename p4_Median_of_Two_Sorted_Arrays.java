0a33b3cff9178f963d037982a647700b753a79dc
Bo Zhou <bzhou2@ualberta.ca>
2018-02-04 22:38:46 -0700

    finish problem 4 by Java

---
+++ p4_Median_of_Two_Sorted_Arrays.java
@@ -0,0 +1,55 @@
+/**
+ - Leetcode problem: 70
+
+ - Difficulty: Easy
+
+ - Brief problem description:
+
+ You are climbing a stair case. It takes n steps to reach to the top.
+
+ Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ Note: Given n will be a positive integer.
+
+ Example 1:
+
+ Input: 2
+ Output:  2
+ Explanation:  There are two ways to climb to the top.
+
+ 1. 1 step + 1 step
+ 2. 2 steps
+ Example 2:
+
+ Input: 3
+ Output:  3
+ Explanation:  There are three ways to climb to the top.
+
+ 1. 1 step + 1 step + 1 step
+ 2. 1 step + 2 steps
+ 3. 2 steps + 1 step
+
+
+ - Solution Summary:
+ If there are 8 stairs, it will be the [result of 6 stairs + 2 steps] + [result of 7 stairs + 1 step]
+
+ - Used Resources:
+
+ --- Bo Zhou
+ */
+
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<Integer> sortedNumber = new ArrayList;
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length || j < nums2.length) {
+            if (i < nums1.length && j < nums2.length){
+                sortedNumber
+            }
+        }
+
+        return result[n];
+    }
+}
