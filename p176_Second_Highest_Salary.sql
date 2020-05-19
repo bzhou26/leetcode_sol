671e02300136a5d76b509a1cd0c2a32f8c7ca34f
Bo Zhou <bzhou2@ualberta.ca>
2020-05-19 03:23:07 -0600

    update problem 176

--- p176_Second_Highest_Salary.sql
+++ p176_Second_Highest_Salary.sql
@@ -16,4 +16,4 @@
 -- +---------------------+
 
 # Write your MySQL query statement below
-SELECT IFNULL((SELECT DISTINCT Salary from Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1), NULL)AS SecondHighestSalary;
+SELECT IFNULL((SELECT DISTINCT Salary from Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary;
