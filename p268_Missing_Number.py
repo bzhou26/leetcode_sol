18f2d627ffc452bc3b99993b7a7bd20d90ee1232
Bo Zhou <bzhou82@gatech.edu>
2020-07-02 20:35:02 -0600

    finish problem 268

---
+++ p268_Missing_Number.py
@@ -0,0 +1,36 @@
+'''
+- Leetcode problem: 268
+
+- Difficulty: Easy
+
+- Brief problem description:
+
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space
+complexity?
+
+- Solution Summary:
+
+- Used Resources:
+
+--- Bo Zhou
+'''
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        missing = len(nums)
+        for i in range(len(nums)):
+            missing ^= nums[i] ^ i
+
+        return missing
