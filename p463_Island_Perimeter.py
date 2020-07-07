ad81b39987fca56378a29613cd9404dc384753d8
Bo Zhou <bzhou82@gatech.edu>
2020-07-07 01:28:22 -0600

    new solution for problem 463

--- p463_Island_Perimeter.py
+++ p463_Island_Perimeter.py
@@ -13,4 +13,21 @@ class Solution(object):
                 if h != 0 and grid[w][h - 1] == 1: add_length -= 2
                 sum += add_length
         return sum
-                
+
+
+class Solution2:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        total = 0
+        direct = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    cur = 4
+                    for dx, dy in direct:
+                        new_x = i + dx
+                        new_y = j + dy
+                        if  0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]):
+                            if grid[new_x][new_y] == 1:
+                                cur -= 1
+                    total += cur
+        return total
