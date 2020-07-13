9abc4e248459d63694553c5be9f4674115e28f83
Bo Zhou <bzhou82@gatech.edu>
2020-07-13 03:06:53 -0600

    finish problem 100

---
+++ p100_Same_Tree.py
@@ -0,0 +1,67 @@
+'''
+- Leetcode problem: 1304
+
+- Difficulty: Easy
+
+- Brief problem description:
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+
+- Solution Summary:
+
+- Used Resources:
+
+--- Bo Zhou
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        return self.dfs(p, q)
+
+    def dfs(self, node1, node2):
+        if node1 is None and node2 is None:
+            return True
+        if node1 is None or node2 is None:
+            return False
+        if node1.val != node2.val:
+            return False
+        if self.dfs(node1.left, node2.left):
+            return self.dfs(node1.right, node2.right)
+        return False
